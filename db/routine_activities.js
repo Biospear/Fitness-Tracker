bd9534eaa6d0d89e4e9623ab10c367446d6148bf
Jaden <jadenshort231@gmail.com>
2022-04-06 11:14:05 -0500

    finished add activity to routine

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -1,20 +1,24 @@
 const client = require("./client");
 
-
 async function getRoutinesWithoutActivities() {
-    try {
-        const { rows } = await client.query(
-          `SELECT *
-          FROM routines;
+  try {
+    const { rows } = await client.query(
+      `SELECT *
+          FROM routines
         `
-        );
-    
-        return rows;
-      } catch (error) {
-        throw error;
-      }
+    );
+    console.log("These our Routines without Activities", rows);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
 }
 
+// function will be a join
+//routineId: bicepRoutine.id,
+//  activityId: bicep1.id,
+//  count: 10,
+//   duration: 5,
 
 async function addActivityToRoutine({
   routineId,
@@ -22,20 +26,23 @@ async function addActivityToRoutine({
   duration,
   count,
 }) {
-    try {
-        const { rows } = await client.query(
-          `
-          INSERT INTO routine_activities("routineId", "activityId", duration, count)
-          VALUES ($1, $2, $3, $4)
-          `,
-        //   ON CONFLICT ("routineId", "activityId") DO NOTHING;
-          [routineId, activityId, duration, count]
-        );
-    
-        return rows;
-      } catch (error) {
-        throw error;
-      }
+  try {
+    const {
+      rows: [routineActivities],
+    } = await client.query(
+      `
+              INSERT INTO routine_activities("routineId", "activityId", duration, count)
+              VALUES ($1, $2, $3, $4)
+              RETURNING *;
+              `,
+      //   ON CONFLICT ("routineId", "activityId") DO NOTHING;
+      [routineId, activityId, duration, count]
+    );
+
+    return routineActivities;
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
