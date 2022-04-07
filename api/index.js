d6b7f884492ea0bd9872e17bf107c26302310dfd
Hays <haysbondy@icloud.com>
2022-04-07 17:51:32 -0500

    finished API /health

--- api/index.js
+++ api/index.js
@@ -1,3 +1,23 @@
 // create an api router
 // attach other routers from files in this api directory (users, activities...)
 // export the api router
+
+const express = require("express");
+const apiRouter = express.Router();
+// require("dotenv").config();
+
+apiRouter.get("/health", (req, res, next) => {
+    res.send({
+        name: 'Health',
+        message: "all is well"
+    });
+});
+
+apiRouter.use((error, req, res, next) => {
+    res.send({
+      name: error.name,
+      message: error.message,
+    });
+  });
+
+module.exports = apiRouter;
--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // create the express server here
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const apiRouter = require("./api");
+// require("dotenv").config();
+// const morgan = require("morgan");
+// app.use(morgan("dev"));
+// app.use(express.json());
+
+const { PORT = 3000 } = process.env;
+app.use(cors());
+
+app.use("/api", apiRouter);
+
+app.listen(PORT, () => {
+  console.log("The server is up on port", PORT);
+});
+
+app.listen(80, () => {
+  console.log("CORS-enabled web server listening on port 80");
+});
